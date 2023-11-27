eb063c8e91e8cacbc15a5496994d95d2f07f755a
Hannah Ponting <hannahponting01@gmail.com>
2023-11-27 11:29:34 +0000

    Add delete method

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -2,6 +2,8 @@ package com.recipe.controllers;
 
 import com.recipe.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,10 @@ public class RecipeController {
     @Autowired
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") long id) {
+        recipeService.deleteById(id);
     }
 }
--- src/main/java/com/recipe/services/RecipeService.java
+++ src/main/java/com/recipe/services/RecipeService.java
@@ -1,7 +1,13 @@
 package com.recipe.services;
 
+import com.recipe.dataaccess.RecipeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class RecipeService {
+
+    public void deleteById(long id) {
+        recipeRepository.deleteById(id);
+    }
 }
