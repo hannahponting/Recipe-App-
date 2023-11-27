00d6411cb817420f974321a8c6bade5a1e6ba6fd
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-11-27 11:14:12 +0000

    controller basic code added

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -1,7 +1,17 @@
 package com.recipe.controllers;
 
+import com.recipe.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/recipes")
 public class RecipeController {
+    RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
 }
