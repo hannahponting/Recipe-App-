4dd5a8554c1ad5a680e65669f0d8cc77db0d7a2a
rui jing <jingrui5@163.com>
2023-11-27 11:21:44 +0000

    create request methods in the controller

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -1,9 +1,14 @@
 package com.recipe.controllers;
 
+import com.recipe.entities.Recipe;
 import com.recipe.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/recipes")
@@ -14,4 +19,25 @@ public class RecipeController {
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
+    @GetMapping("")
+    public Iterable<Recipe> getRecipe(){
+        return recipeService.findAll();
+    }
+
+    @GetMapping("/{recipeId}")
+    public Recipe getRecipeById(@PathVariable Long recipeId){
+        Recipe recipe = recipeService.getrecipeById(recipeId);
+        if (recipe == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe not found");
+
+        return recipe;
+    }
+
+
+
+
+
+
+
 }
