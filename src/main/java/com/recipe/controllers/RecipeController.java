7b805791abfef3b0197bd3b1b1ebf5790370333b
Edmund Woodfield <edmundwoodfield@gmail.com>
2023-11-27 11:20:15 +0000

    service basic code added

--- src/main/java/com/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipe/controllers/RecipeController.java
@@ -1,9 +1,13 @@
 package com.recipe.controllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.recipe.entities.Recipe;
 import com.recipe.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+
 
 @RestController
 @RequestMapping("/recipes")
@@ -13,5 +17,18 @@ public class RecipeController {
     @Autowired
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+    }
+
+    @PostMapping("")
+    @Operation(summary = "create a new user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
+        Recipe newRecipe;
+        try {
+            newRecipe = recipeService.addRecipe(recipe);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+        return newRecipe;
     }
 }
--- src/main/java/com/recipe/services/RecipeService.java
+++ src/main/java/com/recipe/services/RecipeService.java
@@ -1,7 +1,17 @@
 package com.recipe.services;
 
+import com.recipe.dataaccess.RecipeRepository;
+import com.recipe.entities.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
+    RecipeRepository recipeRepository;
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository){
+        this.recipeRepository = recipeRepository;
+    }
+    public Recipe addRecipe(Recipe recipe) {
+    }
 }
