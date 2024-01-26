5d6f95fdcb29bf2e0a09dd3f4f77d7583a5d4a44
Hannah Ponting <hannahponting01@gmail.com>
2024-01-26 16:10:10 +0000

    change to corsconfig

--- src/main/java/com/recipe/CorsConfig.java
+++ src/main/java/com/recipe/CorsConfig.java
@@ -33,6 +33,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // Adjust the origin to match your React app's URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
